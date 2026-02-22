ed958e29e227ad57ef134799ab172e29c070231c
jaimin <115662244+jaimindevops@users.noreply.github.com>
2026-02-21 19:54:34 -0500

    Delete exercise/threshold_alert.py

--- exercise/threshold_alert.py
+++
@@ -1,9 +0,0 @@
-cpu_metrics = [45, 82, 100, 30, 95, 12]
-
-for value in cpu_metrics:
-    if value == 100:
-        print("CRITICAL")
-    elif value > 80:
-        print("WARNING")
-    else:
-        print("OK")
